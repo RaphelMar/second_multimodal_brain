841d5ee8e0468b573034a30a519271aea54fc379
Raphael Resende <raphael.resende.business@gmail.com>
2026-03-26 22:04:15 -0300

    feat: optimize semantic chunking and implement batch ingestion

---
+++ ingestion_lote.py
@@ -0,0 +1,19 @@
+import polars as pl
+from src.ingestion.pipelines import youtube_pipeline
+
+df = (
+    pl.read_excel("data/temp/links.xlsx")
+    .slice(0, 11)
+)
+
+for row in df.iter_rows(named= True):
+    URL = row["Link"]
+    CATEGORIA = row["Categoria"]
+
+    print("\n")
+    print("=" * 50)
+    n_chunks = youtube_pipeline.run(url= URL, category= CATEGORIA)
+    print(f"Ingestão concluída! {n_chunks} fragmentos armazenados na base de conhecimento.")
+    print("=" * 50)
+    print("\n")
+
